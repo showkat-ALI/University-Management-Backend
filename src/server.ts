c56fad6d2d955fcb0675fb2681c4f1f5beb9f2bb
Showkat  Ali <uniqueshowkat114724@gmail.comi>
2023-06-02 01:36:14 +0600

    testing

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
+import corse from 'cors'
+import express, { Request } from 'express'
+//mideleware setup
+
+app.use(corse())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+//main function
+
+async function startServer() {
+  try {
+    await mongoose.connect(config.dataBaseUrl as any)
+    console.log('Connected to database')
+    app.listen(config.port, () => {
+      console.log(`University of coder is running port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startServer()
